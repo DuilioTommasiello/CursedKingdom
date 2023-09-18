b1e5ae6d4eaf750a91764f66268d6da3a26bbf1a
chispitasMB <107868815+chispitasMB@users.noreply.github.com>
2023-09-18 19:52:27 -0300

    Sprites Lady Ferrana
    Cree la animacion de movimiento de lady ferrana, ademas corregi el nombre de algunos scripts que estaban alrevez

--- Assets/Scripts/character scripts/ScriptAnimations.cs
+++ Assets/Scripts/character scripts/ScriptAnimations.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MarkusAnimations : MonoBehaviour
+public class ScriptAnimations : MonoBehaviour
 {
     [Header("Animator")]
     public Animator playerAnimator;
