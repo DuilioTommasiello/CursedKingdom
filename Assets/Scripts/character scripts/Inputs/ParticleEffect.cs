0247f2ded7df621ab6e39c58d813635dcca5425f
VinnyArr <bauti.iribarren@gmail.com>
2023-10-24 20:32:08 -0300

    Efecto de movimiento + Assets decoracion
    Efecto particulas de los personajes y Assets en el hall de entrada

---
+++ Assets/Scripts/character scripts/Inputs/ParticleEffect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffect : MonoBehaviour
+{
+    public ParticleSystem DustVertical;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(Input.GetKeyDown(KeyCode.DownArrow)|| Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            CreateDustVertical();
+        }
+       
+
+        
+        
+    }
+    void CreateDustVertical()
+    {
+        DustVertical.Play();
+    }
+    
+}
