6cfbd8d4737bc19c8af2162a8e40d0cc9fa9275d
chispitasMB <107868815+chispitasMB@users.noreply.github.com>
2023-11-28 01:46:06 -0300

    estado del boss
    boss semi terminado

--- Assets/Prefab/Enemies/Boss/SecondState.cs
+++ Assets/Prefab/Enemies/Boss/SecondState.cs
@@ -7,33 +7,57 @@ public class SecondState : MonoBehaviour
     [SerializeField] EnemyLife enemySc;
     [SerializeField] List<Transform> wayPoints;
     public int nextPos = 0;
-    public int timer = 0;
+    public float timer = 0;
+    public float finaltimer = 10;
+    private Collider2D boxCollider;
 
-    private void Awake()
+    private void Start()
+
     {
         enemySc = GetComponent<EnemyLife>();
+        boxCollider = GetComponent<Collider2D>();
     }
-
     private void Update()
     {
-        patroll();
+        if (enemySc.EnemLife == 50 && wayPoints.Count != 0)
+        {
+            secondState();
+        }
     }
-
-    void patroll()
+    void secondState()
     {
-        if(enemySc.EnemLife < enemySc.EnemLife/2)
+        if (enemySc.EnemLife < enemySc.EnemLife / 2)
+            Debug.Log("secondState");
+        timer += Time.deltaTime;
+        if (timer >= finaltimer)
         {
             Debug.Log("hola marcianete");
-        transform.position = wayPoints[0].position;
-        timer++;
-        
-        if (timer >= 3 )
+            transform.position = wayPoints[0].position;
+            timer++;
+
+            if (timer >= 3)
+            {
+                timer = 0f;
+            TeleportToNextWaypoint();
+            }
+        }
+    }
+    void TeleportToNextWaypoint()
+    {
+        Debug.Log("TeleportToNextWaypoint");
+        if (boxCollider != null)
         {
             timer = 0;
             nextPos++;
             transform.position = wayPoints[nextPos].position;
-        
+
+            boxCollider.enabled = false;
         }
+        nextPos++;
+        if (nextPos >= wayPoints.Count)
+        {
+            nextPos = 0;
         }
+        transform.position = wayPoints[nextPos].position;
     }
 }
