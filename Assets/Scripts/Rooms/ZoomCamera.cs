a6f9c77dd73652e19a1ed75f11e911031701a061
VinnyArr <bauti.iribarren@gmail.com>
2023-11-28 01:48:10 -0300

    Boss Scene
    Arreglos definitivos

---
+++ Assets/Scripts/Rooms/ZoomCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+  
+public class ZoomCamera : MonoBehaviour
+{
+    public bool ZoomActive;
+    public Vector3[] Target;
+    public CinemachineVirtualCamera virtualCamera;
+    public float Speed;
+
+    private void LateUpdate()
+    {
+        if (ZoomActive == true)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, 9f, Speed);
+        }
+        else if (ZoomActive == false)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, 6f, Speed);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ZoomActive = true;
+        }
+    }
+}
+
