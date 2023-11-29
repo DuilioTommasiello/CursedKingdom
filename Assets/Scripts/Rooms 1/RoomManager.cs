9773e38528af0f7dd817ae0126449921918dc472
chispitasMB <107868815+chispitasMB@users.noreply.github.com>
2023-11-28 23:13:28 -0300

    fogs terminados
    falta pulir pero ya fue que la chupe balshet

--- Assets/Scripts/Rooms 1/RoomManager.cs
+++ Assets/Scripts/Rooms 1/RoomManager.cs
@@ -23,8 +23,10 @@ public class RoomManager : MonoBehaviour
     private void Update()
     {
         bool InsideARoom = false;
+        CountEnemiesInRoom();
+        destoyfogs();
 
-        foreach(Collider2D collider in rooms)
+        foreach (Collider2D collider in rooms)
         {
             #region roomsIndex 
             if (collider.bounds.Contains(player.transform.position))
@@ -81,7 +83,8 @@ public class RoomManager : MonoBehaviour
              break;
 
             case 3:
-                if (Fogs[2] != null)
+
+                    if (Fogs[2] != null)
                     Fogs[2].SetActive(true);
 
                 if (Fogs[3] != null)
@@ -110,5 +113,67 @@ public class RoomManager : MonoBehaviour
                 break;
         }
         #endregion
+    }
+    private void CountEnemiesInRoom()
+    {
+
+        Collider2D currentRoomCollider = null;
+
+        foreach (Collider2D collider in rooms)
+        {
+            if (collider.bounds.Contains(player.transform.position))
+            {
+                currentRoomCollider = collider;
+                break;
+            }
+        }
+
+        if (currentRoomCollider != null)
+        {
+            int enemyCount = 0;
+
+            Collider2D[] enemiesInRoom = Physics2D.OverlapCircleAll(currentRoomCollider.bounds.center, currentRoomCollider.bounds.size.x / 2f, 1 << 6);
+            foreach (Collider2D enemyCollider in enemiesInRoom)
+            {
+                if (enemyCollider.gameObject.layer == 6)
+                {
+                    enemyCount++;
+                }
+            }
+
+            EnemyOnRoom = enemyCount; 
+        }
+        else
+        {
+            EnemyOnRoom = 0; 
+        }
+    }
+    
+    void destoyfogs()
+    {
+        if(_whichRoom == 1 && EnemyOnRoom == 0 )
+        {
+            Destroy(Fogs[0]);
+        }
+        if (_whichRoom == 2 && EnemyOnRoom == 0)
+        {
+            Destroy(Fogs[1]);
+            Destroy(Fogs[2]);
+        }
+        if (_whichRoom == 3 && EnemyOnRoom == 0)
+        {
+            Destroy(Fogs[2]);
+            Destroy(Fogs[3]);
+        }
+        if (_whichRoom == 4 && EnemyOnRoom == 0)
+        {
+            Destroy(Fogs[3]);
+            Destroy(Fogs[4]);
+        }
+        if (_whichRoom == 4 && EnemyOnRoom == 0)
+        {
+            Destroy(Fogs[4]);
+            Destroy(Fogs[1]);
+        }
     }
 }
